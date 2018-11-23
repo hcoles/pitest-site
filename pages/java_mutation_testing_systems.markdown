hcoles/pitest-site

commit 69c1f5b51ce983c35fb03fa678a8e04a487489a6

Use a shorter syntax to link to footnotes
This is especially helpful in tables, were long lines are a bit
problematics. For consistency's sake, the change was applied everywhere.

pages/java_mutation_testing_systems.markdown
@@ -42,7 +42,7 @@ Jester was the first open source mutation system for Java, but does not appear t
 
 WebSite : [http://jester.sourceforge.net/](http://jester.sourceforge.net/)
 
-Simple Jester is a variant of the original Jester by the same author who describes it as easier to use, but slower <a name="fnref1" id="fnref1"></a> <a href="#fn1"><sup>1</sup></a>.
+Simple Jester is a variant of the original Jester by the same author who describes it as easier to use, but slower <sup id="fnref1">[1]((#fn1))</sup>.
 
 It appears to no longer be actively developed or supported.
 
@@ -131,7 +131,7 @@ Of these libraries, BCEL is no longer actively developed or maintained.
 * Generally much faster
 * Generally easier to integrate into a build
 * Can potentially create mutants without access to source files
-* Same mutation operators can in theory work for other JVM languages <a name="fnref2" id="fnref2"></a> <a href="#fn2"><sup>2</sup></a>
+* Same mutation operators can in theory work for other JVM languages <sup id="fnref2">[2](#fn2)</sup>
 
 #### Cons
 
@@ -203,7 +203,7 @@ Optimisations may also be implemented to choose an optimal running order for the
 
 #### Cons
 
-* Some overhead required to measure coverage <a name="fnref3" id="fnref3"></a> <a href="#fn3"><sup>3</sup></a>.
+* Some overhead required to measure coverage <sup id="fnref3">[3](#fn3)</sup>.
 
 ## Mutant insertion
 
@@ -352,14 +352,14 @@ by other tools, eg XML, RDMS etc.
 
 ### Classification and details
 
-| System                          | Generation | Selection    | Insertion     | Detection   | Output | SF    | License | Mailing List                                                            |
-|---------------------------------|------------|--------------|---------------|-------------|--------|-------|---------|-------------------------------------------------------------------------|
-| [Jester](#jester)               | Source     | Naive ?      | Naive ?       | Naive ?     | **AS** | N     | MIT     | N                                                                       |
-| [Simple Jester](#simple-jester) | Source     | Naive ?      | Naive ?       | Naive ?     | **AS** | N     | MIT     | N                                                                       |
-| [Jumble](#jumble)               | BCEL       | Convention   | NDClassloader | **EE Fine** | T      | N     | APCH2   | **Y** <a name="fnref4" id="fnref4"></a> <a href="#fn4"><sup>4</sup></a>. |
-| [PIT](#pit)                     | ASM        | **Coverage** | Instrument    | **EE Fine** | **AS** | **Y** | APCH2   | **Y** <a name="fnref5" id="fnref5"></a> <a href="#fn5"><sup>5</sup></a>. |
-| [µJava](#µjava)                 | Source ?   | Manual       | N/A           | N/A         | **AS** | N     | ?       | N                                                                       |
-| [javaLanche](#javalanche)       | ASM        | **Coverage** | Schemata      | EE Fine ?   | P      | **Y** | LGPL    | N                                                                       |
+| System                          | Generation | Selection    | Insertion     | Detection   | Output | SF    | License | Mailing List                            |
+|---------------------------------|------------|--------------|---------------|-------------|--------|-------|---------|-----------------------------------------|
+| [Jester](#jester)               | Source     | Naive ?      | Naive ?       | Naive ?     | **AS** | N     | MIT     | N                                       |
+| [Simple Jester](#simple-jester) | Source     | Naive ?      | Naive ?       | Naive ?     | **AS** | N     | MIT     | N                                       |
+| [Jumble](#jumble)               | BCEL       | Convention   | NDClassloader | **EE Fine** | T      | N     | APCH2   | **Y** <sup id="fnref4">[4](#fn4)</sup>. |
+| [PIT](#pit)                     | ASM        | **Coverage** | Instrument    | **EE Fine** | **AS** | **Y** | APCH2   | **Y** <sup id="fnref5">[5](#fn5)</sup>. |
+| [µJava](#µjava)                 | Source ?   | Manual       | N/A           | N/A         | **AS** | N     | ?       | N                                       |
+| [javaLanche](#javalanche)       | ASM        | **Coverage** | Schemata      | EE Fine ?   | P      | **Y** | LGPL    | N                                       |
 {:.table}
 
 ### Java and Test framework support
@@ -385,14 +385,14 @@ by other tools, eg XML, RDMS etc.
 *note that with the exception of PIT little information is available on the compatibility of the various mutation testing systems and
 mocking frameworks. Most mocking systems are implemented with dynamic proxies or custom class loaders, and will probably work across all the mutation testing sytems. The exceptions are Powermock and JMockit where issues might be encountered.*
 
-| System                          | Ant | Maven | Eclipse                                                               | Powermock | JMock | JMock2 | Mockito | JMockit                                                               | EasyMock                                                            |
-|---------------------------------|-----|-------|-----------------------------------------------------------------------|-----------|-------|--------|---------|-----------------------------------------------------------------------|---------------------------------------------------------------------|
-| [Jester](#jester)               | N   | N     | N                                                                     | ?         | ?     | ?      | ?       | ?                                                                     | ?                                                                   |   
-| [Simple Jester](#simple-jester) | N   | N     | N                                                                     | ?         | ?     | ?      | ?       | ?                                                                     | ?                                                                   |
-| [Jumble](#jumble)               |**Y**| N     | **Y**                                                                 | ?         | **Y** | **Y**  | **Y**   | N <a name="fnref6" id="fnref6"></a> <a href="#fn6"><sup>6</sup></a>.  | ?                                                                   |
-| [PIT](#pit)                     |**Y**|**Y**  | 3rd                                                                   | **Y**     | **Y** | **Y**  | **Y**   | **Y**                                                                 | **Y**                                                               |
-| [µJava](#µjava)                 | N   | N     | 3rd <a name="fnref7" id="fnref7"></a> <a href="#fn7"><sup>7</sup></a> | ?         | ?     | ?      | ?       | ?                                                                     | ?                                                                   |
-| [javaLanche](#javalanche)       | N   | N     | N                                                                     | ?         | ?     | ?      | ?       | N <a name="fnref8" id="fnref8"></a> <a href="#fn8"><sup>8</sup></a>.  | ! <a name="fnref9" id="fnref9"></a> <a href="#fn9"><sup>9</sup></a>.|
+| System                          | Ant | Maven | Eclipse                              | Powermock | JMock | JMock2 | Mockito | JMockit                             | EasyMock                           |
+|---------------------------------|-----|-------|--------------------------------------|-----------|-------|--------|---------|-------------------------------------|------------------------------------|
+| [Jester](#jester)               | N   | N     | N                                    | ?         | ?     | ?      | ?       | ?                                   | ?                                  |   
+| [Simple Jester](#simple-jester) | N   | N     | N                                    | ?         | ?     | ?      | ?       | ?                                   | ?                                  |
+| [Jumble](#jumble)               |**Y**| N     | **Y**                                | ?         | **Y** | **Y**  | **Y**   | N <sup id="fnref6">[6](#fn6)</sup>. | ?                                  |
+| [PIT](#pit)                     |**Y**|**Y**  | 3rd                                  | **Y**     | **Y** | **Y**  | **Y**   | **Y**                               | **Y**                              |
+| [µJava](#µjava)                 | N   | N     | 3rd <sup id="fnref7">[7](#fn7)</sup> | ?         | ?     | ?      | ?       | ?                                   | ?                                  |
+| [javaLanche](#javalanche)       | N   | N     | ?                                    | ?         | ?     | ?      | ?       | N <sup id="fnref8">[8](#fn8)</sup>. | ! <sup id="fnref9">[9](#fn9)</sup>.|
 {:.table}
 
 # Conclusions

_posts/2011-09-26-we_have_a_roadmap.markdown
@@ -6,9 +6,7 @@ tags: roadmap
 layout: post
 ---
 
-Several people have asked if there is a roadmap for PIT, so with a nod to Heisenberg <a href="#fn1"><sup>1</sup></a>
-<a name="fnref1" id="fnref1"></a>
-[now we do](/roadmap/).
+Several people have asked if there is a roadmap for PIT, so with a nod to Heisenberg <sup id="fnref1">[1](#fn1)</sup> [now we do](/roadmap/).
 
 <!-- more -->
 

quickstart/mutators.markdown
@@ -359,10 +359,10 @@ are converted to byte code.
 | Constant Type            | Mutation
 |--------------------------|---
 | `boolean`                | replace the unmutated value `true` with `false` and replace the unmutated value `false` with `true`
-| `integer` `byte` `short` | replace the unmutated value `1` with `0`, `-1` with `1`, `5` with `-1` or otherwise increment the unmutated value by one. <a href="#fn1"><sup>1</sup></a> <a name="fnref1" id="fnref1"></a>
+| `integer` `byte` `short` | replace the unmutated value `1` with `0`, `-1` with `1`, `5` with `-1` or otherwise increment the unmutated value by one. <sup id="fnref1">[1](#fn1)</sup>
 | `long`                   | replace the unmutated value `1` with `0`, otherwise increment the unmutated value by one.
-| `float`                  | replace the unmutated values `1.0` and `2.0` with `0.0` and replace any other value with `1.0` <a href="#fn2"><sup>2</sup></a> <a name="fnref2" id="fnref2"></a>
-| `double`                 | replace the unmutated value `1.0` with `0.0` and replace any other value with `1.0` <a href="#fn3"><sup>3</sup></a> <a name="fnref3" id="fnref3"></a>
+| `float`                  | replace the unmutated values `1.0` and `2.0` with `0.0` and replace any other value with `1.0` <sup id="fnref2">[2](#fn2)</sup>
+| `double`                 | replace the unmutated value `1.0` with `0.0` and replace any other value with `1.0` <sup id="fnref3">[3](#fn3)</sup>
 {:.table}
 
 
@@ -419,7 +419,7 @@ Return Values Mutator (RETURN_VALS)
 **Active by default**
 
 The return values mutator mutates the return values of method calls. Depending
-on the return type of the method another mutation is used.<a href="#fn4"><sup>4</sup></a> <a name="fnref4" id="fnref4"></a>
+on the return type of the method another mutation is used.<sup id="fnref4">[4](#fn4)</sup></a>
 
 | Return Type          | Mutation 
 |----------------------|---

commit a4ae9ea0294c72d92b2d47581f67d1a231f9c2f5

Fix link to first foot note of comparison page
69c1f5b5 broke the link by adding a extra layer of parenthesis.
This commit fixes it.

pages/java_mutation_testing_systems.markdown
@@ -42,7 +42,7 @@ Jester was the first open source mutation system for Java, but does not appear t
 
 WebSite : [http://jester.sourceforge.net/](http://jester.sourceforge.net/)
 
-Simple Jester is a variant of the original Jester by the same author who describes it as easier to use, but slower <sup id="fnref1">[1]((#fn1))</sup>.
+Simple Jester is a variant of the original Jester by the same author who describes it as easier to use, but slower <sup id="fnref1">[1](#fn1)</sup>.
 
 It appears to no longer be actively developed or supported.
 